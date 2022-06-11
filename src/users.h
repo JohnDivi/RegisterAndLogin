bf9a62b8c26ef7a4b0a7ab2b9f4cce80a390a998
JohnDivi <johnbdivinagracia23@gmail.com>
2022-06-11 01:45:54 -0600

    Setup users.h

---
+++ src/users.h
@@ -0,0 +1,21 @@
+#ifndef USERS_H
+#define USERS_H
+
+#include "database.h"
+#include <vector>
+#include <string>
+
+class User {
+
+    private:
+        std::string username;
+        std::vector<std::string> entries;
+
+    public:
+        User(std::string username);
+        void registerUser(Database &database);
+        void loginUser(Database database);
+
+}
+
+#endif
