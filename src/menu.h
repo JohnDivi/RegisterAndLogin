1c43d8c81691b93413fb3e5ed1c7b6f296bde5fa
JohnDivi <johnbdivinagracia23@gmail.com>
2022-06-11 13:26:10 -0600

    Updated menu.h to follow README

--- src/menu.h
+++ src/menu.h
@@ -1,21 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
 
-#include <vector>
+#include <unordered_map>
 #include <string>
 
 class Menu {
 
     private:
-        std::vector<std::string> content;
-        std::vector<Menu> otherMenus;
+        std::unordered_map<std::string, std::string> menus;
 
     public:
-        Menu(int menuSize);
-        void updateMenuTitle(std::string title);
-        void updateMenuChoice(int choice, std::string details);
-        void showMenu();
-        Menu getMenu(int choice);
+        Menu();
+        int getMenuChoice(std::string menu);
 
 }
 
